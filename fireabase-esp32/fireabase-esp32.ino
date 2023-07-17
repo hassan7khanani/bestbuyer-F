fab52c78aa3597df90c60963c7e1f8df71febfc8
hassan7khanani <123535083+hassan7khanani@users.noreply.github.com>
2023-07-17 22:12:12 +0500

    Update fireabase-esp32.ino

--- fireabase-esp32/fireabase-esp32.ino
+++ fireabase-esp32/fireabase-esp32.ino
@@ -27,25 +27,25 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define LOGO_WIDTH    16
 
 bool deviceregistration=false;
+String comment;
 
-
-static const unsigned char PROGMEM logo_bmp[] =
-{ 0b00000000, 0b11000000,
-  0b00000001, 0b11000000,
-  0b00000001, 0b11000000,
-  0b00000011, 0b11100000,
-  0b11110011, 0b11100000,
-  0b11111110, 0b11111000,
-  0b01111110, 0b11111111,
-  0b00110011, 0b10011111,
-  0b00011111, 0b11111100,
-  0b00001101, 0b01110000,
-  0b00011011, 0b10100000,
-  0b00111111, 0b11100000,
-  0b00111111, 0b11110000,
-  0b01111100, 0b11110000,
-  0b01110000, 0b01110000,
-  0b00000000, 0b00110000 };
+// static const unsigned char PROGMEM logo_bmp[] =
+// { 0b00000000, 0b11000000,
+//   0b00000001, 0b11000000,
+//   0b00000001, 0b11000000,
+//   0b00000011, 0b11100000,
+//   0b11110011, 0b11100000,
+//   0b11111110, 0b11111000,
+//   0b01111110, 0b11111111,
+//   0b00110011, 0b10011111,
+//   0b00011111, 0b11111100,
+//   0b00001101, 0b01110000,
+//   0b00011011, 0b10100000,
+//   0b00111111, 0b11100000,
+//   0b00111111, 0b11110000,
+//   0b01111100, 0b11110000,
+//   0b01110000, 0b01110000,
+//   0b00000000, 0b00110000 };
 
 
 WiFiClient espClient;
@@ -81,125 +81,63 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
-    display.setTextSize(1);
-    display.setTextColor(WHITE);
-    display.setCursor(0, 10);
-    display.println("Connecting to Wifi");
-    display.display();
-    delay(2000);
-    display.clearDisplay();
+    displayonscreen("Connecting to Wifi");
+
 
   }
-
-    // bool res;
-    // res = wm.autoConnect(); // auto generated AP name from chipid
-    // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
-    // res = wm.autoConnect("IoTPlant","12345678"); // password protected ap
-
-    // if(!res) {
-    //     Serial.println("Failed to connect");
-    //     // ESP.restart();
-    // } 
-    // else {
-    //     //if you get here you have connected to the WiFi    
-    //     Serial.println("connected...yeey :)");
-    // }
     Serial.println("Connected to WiFi");
-    display.setTextSize(1);
-    display.setTextColor(WHITE);
-    display.setCursor(0, 10);
-    display.println("Connected to Wifi");
-    display.display();
-    // display.clearDisplay();
-    delay(2000);
-    display.clearDisplay();
+    displayonscreen("Connected to WiFi");
+    Serial.println(WiFi.macAddress());
+    macAddress = WiFi.macAddress();
+    result=a+macAddress.substring(0,10);
 
+    displayonscreen("Connecting to MQTT");
+    Serial.println("Connecting to MQTT");
+    client.setServer(mqtt_server, 1883);
+    client.setCallback(callback);
+    Serial.println("MQTT Connected");
+    displayonscreen("Connected to MQTT");
+    
 
-    // display.setTextSize(1);
-    // display.setTextColor(WHITE);
-    // display.setCursor(0, 5);
-    // display.println("Checking Device is registered on the Database");
-    // display.display();
-    // // display.clearDisplay();
-    // delay(2000);
-    // // display.clearDisplay();
-
-    display.setTextSize(1);
-    display.setTextColor(WHITE);
-    display.setCursor(0, 5);
-    display.println("Connecting to MQTT");
-    display.display();
-    // display.clearDisplay();
-    delay(2000);
-    display.clearDisplay();
-
-
-  Serial.println(WiFi.macAddress());
-  macAddress = WiFi.macAddress();
-  result=a+macAddress.substring(0,10);
-  Serial.println("Connecting to MQTT");
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-  Serial.println("MQTT Connected");
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 5);
-  display.println("MQTT Connected");
-  display.display();
-    // display.clearDisplay();
-  delay(2000);
-    // display.clearDisplay();
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-
-
-
-  // display.display();
-  // delay(2000);
-  // Clear the buffer
-  // display.clearDisplay();
-
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  Serial.printf("Get string... %s\n", Firebase.getString(firebaseData, F("/Devices/macaddress/")) ? firebaseData.to<const char *>() : firebaseData.errorReason().c_str());
-  // jsonData=Firebase.getString(firebaseData, F("/Devices/macaddress")) ? firebaseData.to<const char *>() : firebaseData.errorReason().c_str());
-  jsonData=Firebase.getString(firebaseData, F("/Devices/macaddress")) ? firebaseData.to<const char *>() : firebaseData.errorReason().c_str();
-
-  DeserializationError error = deserializeJson(doc, jsonData);
+    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+    Serial.printf("Get string... %s\n", Firebase.getString(firebaseData, F("/Devices/macaddress/")) ? firebaseData.to<const char *>() : firebaseData.errorReason().c_str());
+    jsonData=Firebase.getString(firebaseData, F("/Devices/macaddress")) ? firebaseData.to<const char *>() : firebaseData.errorReason().c_str();
+    DeserializationError error = deserializeJson(doc, jsonData);
 
   // Check if there was an error in parsing the JSON
-  if (error) {
-    Serial.print("JSON parsing failed: ");
-    Serial.println(error.c_str());
-    return;
-  }
-  for (JsonPair kv : doc.as<JsonObject>()) {
-    String key = kv.key().c_str();
-    String value = kv.value().as<String>();
-    
-    Serial.print("Key: ");
-    Serial.print(key);
-    Serial.print(" | Value: ");
-    Serial.println(value);
-    if (value == macAddress) {
-      Serial.print("Match found! Key: ");
-      Serial.println(key);
-      matchFound = true;
-      display.setTextSize(1);
-      display.setTextColor(WHITE);
-      display.setCursor(0, 5);
-      display.println("Device already registered");
-      display.display();
-      delay(2000);
-      display.clearDisplay();
-      break;
+    if (error) {
+        Serial.print("JSON parsing failed: ");
+        Serial.println(error.c_str());
+        return;
     }
-  }
+    for (JsonPair kv : doc.as<JsonObject>()) {
+        String key = kv.key().c_str();
+        String value = kv.value().as<String>();
+        
+        Serial.print("Key: ");
+        Serial.print(key);
+        Serial.print(" Value: ");
+        Serial.println(value);
+        if (value == macAddress) {
+        Serial.print("Match found! Key: ");
+        Serial.println(key);
+        matchFound = true;
+        displayonscreen("Device already registered");
+        deviceregistration=true;
+        break;
+        }
+    }
+      displayonscreen("Register device on App");
+      displayonscreen("mac: "+macAddress);
+      Serial.println("mac: "+ macAddress );
 
-    if (!matchFound and deviceregistration==true) {
+  if (!matchFound and deviceregistration==true) {
     
     doc[result] = macAddress;
     Serial.println("No match found. Added value with key 'new_device'.");
     firebasePath = "/Devices/macaddress/" + result;
     Serial.printf("Set string... %s\n", Firebase.setString(firebaseData, firebasePath, macAddress) ? "ok" : firebaseData.errorReason().c_str());
+    displayonscreen("Device Registered Successfully");
     deviceregistration=false;
 
   }
@@ -263,7 +201,20 @@ void callback(char* topic, byte* message, unsigned int length)
 if (String(topic) == "DEVICEONBOARDING" ) 
 {
    Serial.println("message received on topic: DEVICEONBOARDING ");
-   deviceregistration=doc1["registered"]:
+   deviceregistration=doc1["registered"];
   
 }
+}
+
+void displayonscreen(String comment)
+{
+      display.setTextSize(1);
+      display.setTextColor(WHITE);
+      display.setCursor(0, 5);
+      display.println(comment);
+      display.display();
+      delay(2000);
+      display.clearDisplay();
+
+
 }
